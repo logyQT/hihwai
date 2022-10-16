4d3f0b5a11a3ed7d56a215c951c1eb8127abec18
igormlshkv <115819101+igormlshkv@users.noreply.github.com>
2022-10-16 21:37:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ var state=0;
 function colorchanger2000(){
     if(state==0){
         var str = document.querySelector(".Essa");
-        str.style.color="RGB(245, 66, 170)";
+        str.style.color="RGB(196, 8, 36)";
         console.log(str);
         state=1;
     }
     else if(state==1){
         var str = document.querySelector(".Essa");
-        str.style.color="black";
+        str.style.color="yellow";
         console.log(str);
         state=0;
     }
